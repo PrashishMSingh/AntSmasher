368f682912f872228dc79861bd86e292771345fc
PrashishMSingh <prashishMSingh@gmail.com>
2019-10-09 21:50:27 +0545

    initial commit

---
+++ js/bug.js
@@ -0,0 +1,132 @@
+function Bug(xPos, yPos, xSpeed, ySpeed, bugType, bugSize, changeDirAt, CONTAINER_WIDTH, CONTAINER_HEIGHT){
+  this.bugType = bugType;
+  this.bugSize = bugSize;
+  this.xPos = xPos;
+  this.yPos = yPos;
+  this.xSpeed = xSpeed;
+  this.ySpeed = ySpeed;
+  this.changeDirAt = changeDirAt;
+  this.CONTAINER_WIDTH = CONTAINER_WIDTH;
+  this.CONTAINER_HEIGHT= CONTAINER_HEIGHT;
+
+  this.init = function(){
+    this.createBug()
+    this.initialXPos = this.xPos
+    return this;
+  }
+
+  this
+
+  this.createBug = function(){
+    this.bug = document.createElement('div')
+    this.bugHead = document.createElement('div')
+    this.bugBody = document.createElement('div')
+    this.bugAbdomen = document.createElement('div')
+
+    this.styleBug()
+    this.styleBugHead();
+    this.styleBugBody();
+    this.styleBugAbdomen();
+  }
+
+  this.move=function(incrementMiss){
+    if((this.xPos + this.bugSize) >= this.CONTAINER_WIDTH){
+      this.xSpeed = -this.xSpeed;
+    }else if(this.xPos < 0){
+      this.xSpeed = Math.abs(this.xSpeed)
+    }
+    var bugHeight = parseInt(this.bugHead.style.height) + parseInt(this.bugBody.style.height) + parseInt(this.bugAbdomen.style.height)
+
+    if((this.yPos + bugHeight) >= this.CONTAINER_HEIGHT){
+      incrementMiss(this)
+    }
+
+    if((this.xPos - this.initialXPos) > this.changeDirAr || (this.xPos - this.initialXPos) < 0){
+      this.xSpeed = -(this.xSpeed)
+    }
+    this.xPos = this.xPos + this.xSpeed;
+    this.yPos = this.yPos + this.ySpeed;
+  }
+
+  this.draw = function(){
+    this.bug.style.left = this.xPos + 'px';
+    this.bug.style.top = this.yPos + 'px';
+
+    this.bug.appendChild(this.bugAbdomen);
+    this.bug.appendChild(this.bugBody);
+    this.bug.appendChild(this.bugHead);
+  }
+
+  this.styleBug= function(){
+    this.bug.style.position = 'absolute';
+    this.bug.style.display = 'inline-block';
+
+    this.bug.setAttribute('class', this.bugType)
+  }
+
+  this.styleBugHead = function(){
+    if(this.bugType === 'ant'){
+      this.bugHead.style.width = this.bugSize*0.8 + 'px';
+      this.bugHead.style.height = this.bugSize*0.8 + 'px';
+      this.bugHead.style.borderRadius = '50%';
+      this.bugHead.style.backgroundColor = '#343434';
+      this.bugHead.style.margin = 'auto';
+    }
+
+    if(this.bugType === 'wasp'){
+      this.bugHead.style.width = this.bugSize*1.8 + 'px';
+      this.bugHead.style.height = this.bugSize*1.8 + 'px';
+      this.bugHead.style.borderRadius = '50%';
+      this.bugHead.style.backgroundColor = '#CABC0B';
+      this.bugHead.style.margin = 'auto';
+    }
+
+    this.bugHead.setAttribute('class', 'bugHead')
+  }
+
+  this.styleBugBody = function(){
+    if(this.bugType === 'ant'){
+      this.bugBody.style.width = this.bugSize*0.3 + 'px';
+      this.bugBody.style.height = this.bugSize*0.3 + 'px';
+      this.bugBody.style.backgroundColor = '#373629';
+      this.bugBody.style.borderRadius = "50%";
+      this.bugBody.style.margin = 'auto';
+    }
+
+    if(this.bugType === 'wasp'){
+      this.bugBody.style.width = this.bugSize*1.3 + 'px';
+      this.bugBody.style.height = this.bugSize*1.3 + 'px';
+      this.bugBody.style.backgroundColor = '#565555';
+      this.bugBody.style.borderRadius = "50%";
+      this.bugBody.style.margin = 'auto';
+    }
+
+    this.bugBody.setAttribute('class', 'bugBody')
+
+  }
+
+  this.styleBugAbdomen = function(){
+    if(this.bugType === 'ant'){
+      this.bugAbdomen.style.width = this.bugSize + 'px';
+      this.bugAbdomen.style.height = this.bugSize*1.2 + 'px';
+      this.bugAbdomen.style.borderRadius = '50%';
+      this.bugAbdomen.style.backgroundColor = '#4D2F2F';
+      this.bugAbdomen.style.margin = 'auto';
+    }
+
+    if(this.bugType === 'wasp'){
+      this.bugAbdomen.style.width = this.bugSize*2 + 'px';
+      this.bugAbdomen.style.height = this.bugSize*2.5 + 'px';
+      this.bugAbdomen.style.borderRadius = '50%';
+      this.bugAbdomen.style.backgroundColor = '#F0DF07';
+      this.bugAbdomen.style.margin = 'auto';
+    }
+
+    this.bugAbdomen.setAttribute('class', 'bugAbdomen')
+  }
+
+  this.getElement = function(){
+    return this.bug;
+  }
+
+}
---
+++ js/smasherController.js
@@ -0,0 +1,201 @@
+const CONTAINER_WIDTH = 300;
+const CONTAINER_HEIGHT = 700;
+const FRAME_SPEED = 80;
+function SmasherController(containerId, CONTAINER_WIDTH, CONTAINER_HEIGHT){
+
+  this.CONTAINER_WIDTH = CONTAINER_WIDTH;
+  this.CONTAINER_HEIGHT = CONTAINER_HEIGHT;
+  this.containerId = containerId;
+  this.bugList = []
+  this.missCount = 0;
+  this.waspSmashCount = 0;
+  this.antSmashCount = 0;
+  this.gameOver = true;
+  this.difficulty = 5;
+
+  this.init = function(){
+    this.containerElement = document.getElementById(containerId);
+    this.gameOverContainer = this.containerElement.getElementsByClassName('gameOverWrapper')[0];
+    this.levelSelector = this.gameOverContainer.getElementsByClassName('level')[0];
+    this.nextBtn = this.gameOverContainer.getElementsByClassName('nextBtn')[0];
+    this.nextBtn.onclick = this.nextBtnAction;
+    this.pointElement = document.getElementsByClassName('antSmashCount')[0];
+    this.pointElement.innerHTML = this.antSmashCount;
+    this.missedElement = document.getElementsByClassName('missCount')[0];
+    this.missedElement.innerHTML = this.missCount;
+    this.waspElement = document.getElementsByClassName('waspCount')[0];
+    this.waspElement.innerHTML = this.waspSmashCount;
+    return this
+  }
+
+  this.clearDisplay = function(){
+    var wasps = document.getElementsByClassName('wasp')
+    var ants = document.getElementsByClassName('ant')
+    if(wasps.length){
+      for(var i = 0; i <=wasps.lenght; i++){
+          this.containerElement.removeChild(wasps[i])
+      }
+    }
+    if(ants.length){
+      for(var j = 0; j<=ants.length; j++){
+          this.containerElement.removeChild(ants[j])
+      }
+    }
+
+  }
+
+  this.endGame = function(){
+    this.gameOverContainer.style.display = 'block';
+    for(var i = 0; i<this.bugList.length; i++){
+      this.containerElement.removeChild(this.bugList[i].getElement())
+    }
+    this.bugList = []
+    this.waspSmashCount = 0;
+    this.antSmashCount = 0;
+    this.missCount = 0;
+
+  }
+
+  this.nextBtnAction = (function(){
+    this.gameOverContainer.style.display = 'none';
+    this.setDifficulty(this.levelSelector.value)
+    this.gameOver = false;
+    this.waspElement.innerHTML = this.waspSmashCount;
+    this.pointElement.innerHTML = this.antSmashCount;
+    this.missedElement.innerHTML = this.missCount;
+
+
+    this.createBugs()
+    this.updateBugs()
+  }).bind(this)
+
+  this.getElement = function(){
+    return this.containerElement;
+  }
+
+  this.addBug = function(bug){
+    this.bugList.push(bug)
+    this.containerElement.appendChild(bug.getElement())
+    bug.draw()
+  }
+
+  this.removeBug = function(bug){
+    if(!this.gameOver){
+      if(bug instanceof Bug){
+        this.bugList = this.bugList.filter(singleBug => bug !== singleBug)
+        this.containerElement.removeChild(bug.getElement())
+      }
+      else{
+        var bugParts = ['bugHead', 'bugBody', 'bugAbdomen']
+        if(bugParts.includes(bug.className)){
+          var parentElement = bug.parentElement
+          this.bugList = this.bugList.filter(singleBug => parentElement !== singleBug.getElement())
+          this.containerElement.removeChild(parentElement)
+        }
+      }
+    }
+  }
+
+  this.setDifficulty = function(level){
+    this.difficulty = level
+  }
+
+
+  this.incrementMiss = (function(bug){
+    if(bug.bugType === 'ant'){
+      this.missCount ++;
+      this.missedElement.innerHTML = this.missCount
+      this.removeBug(bug)
+      this.containerElement.style.backgroundColor = 'red'
+      if(this.missCount >= 3){
+        this.gameOver = true;
+        this.endGame()
+      }
+    }else if(bug.bugType === 'wasp'){
+      this.containerElement.style.borderBottom = '#070EF0 solid 2px'
+    }
+  }).bind(this)
+
+  this.incrementBugSmash = function(bug){
+    if(bug.parentElement.className === 'ant'){
+        this.antSmashCount++
+        this.pointElement.innerHTML = this.antSmashCount;
+    }
+    else if(bug.parentElement.className === 'wasp'){
+      this.waspSmashCount++
+      this.waspElement.innerHTML = this.waspSmashCount;
+      this.containerElement.style.backgroundColor = 'red'
+      if(this.waspSmashCount >= 3){
+        this.gameOver = true;
+        this.endGame()
+      }
+
+    }
+    this.removeBug(bug)
+  }
+
+  this.createBugs = function(){
+    var bugsNo = 0;
+    var bugCreationInterval = Math.floor(Math.random() * 3000)
+
+    var createBug = setInterval((function(){
+      var maxLimit = 5;
+      var count = this.difficulty
+      if(!this.gameOver){
+        if(this.bugList.length < count){
+          var defaultXPos = Math.floor(Math.random()* CONTAINER_WIDTH)
+          var defaultYPos = 50;
+          var defaultXSpeed = 5;
+          var defaultYSpeed = 5;
+
+          var changeDirAt = Math.floor(Math.random() * CONTAINER_WIDTH)
+          var bugXSpeed = Math.random() + defaultXSpeed;
+          var bugYSpeed = Math.random() + defaultYSpeed;
+          var bugType = 'ant'
+          var waspThreshold = 0.8
+          if(Math.random() > waspThreshold){
+            bugType = 'wasp'
+          }
+          var bug = new Bug(defaultXPos, defaultYPos, bugXSpeed, bugYSpeed, bugType, 20, changeDirAt, CONTAINER_WIDTH, CONTAINER_HEIGHT).init()
+          this.addBug(bug)
+        }
+      }
+      else{
+        clearInterval(createBug)
+      }
+    }).bind(this), bugCreationInterval)
+  }
+
+  this.updateBugs = function(){
+    var updateBug = setInterval( (function(){
+      this.containerElement.style.backgroundColor = '#56A4F3'
+      if(!this.gameOver){
+        for(var i = 0; i<this.bugList.length; i++){
+          this.bugList[i].move(this.incrementMiss)
+          if(this.bugList[i]){
+              this.bugList[i].draw()
+          }
+        }
+      }else{
+        clearInterval(updateBug)
+      }
+    }).bind(this), FRAME_SPEED)
+
+  }
+
+  this.setDefaultContainerStyle = function(){
+    this.containerElement.style.margin = 'auto'
+    this.containerElement.style.borderRadius = '5px'
+    this.containerElement.style.position = 'relative'
+    this.containerElement.style.width = this.CONTAINER_WIDTH + 'px'
+    this.containerElement.style.height = this.CONTAINER_HEIGHT + 'px'
+    this.containerElement.style.backgroundColor = '#56A4F3'
+  }
+
+}
+var controller = new SmasherController('gameContainer', CONTAINER_WIDTH, CONTAINER_HEIGHT).init()
+controller.setDefaultContainerStyle()
+
+controller.getElement().onclick = function(e){
+  controller.incrementBugSmash(e.target)
+}
